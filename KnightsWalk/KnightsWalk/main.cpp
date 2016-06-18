06e46af515979dcc17399ad97fde635efb7afe74
MChomitz - Mike Chomitz <mchomitz {at} gmail {dot} com>
2016-06-18 16:03:53 -0700

    Initial commit, more to come!
    updated .gitignore to ignore extra visual studio files

---
+++ KnightsWalk/KnightsWalk/main.cpp
@@ -0,0 +1,17 @@
+/*
+		Knight's Walk
+		MChomitz - Mike Chomitz
+
+		This program will generate knight's walk(s)
+*/
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is the Knight's Walk Program!";
+	std::cin.get();
+
+	return 0;
+}
